ada2cabe349bfd2f93622d0f99434c1619fc50f1
Anjey Novicki <anjeynov@gmail.com>
2020-03-14 21:52:02 +0300

    feat: part 4 done

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.h
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.h
@@ -1,8 +1,22 @@
 #import <Foundation/Foundation.h>
 
-@interface Parser : NSObject
+@interface Parser : NSObject {
+    NSArray<NSString*> *ringScopes;
+    NSArray<NSString*> *triangleScopes;
+    NSArray<NSString*> *rectangleScopes;
+    
+    
+}
+
+- (id)init;
 
 - (NSArray <NSString*>*)parseString: (NSString*)string;
+
+@end
+
+@interface Parser ()
+
++(NSInteger)searchScopes: (NSArray<NSString*>*)scopes inString:(NSString*) string;
 
 @end
 
--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -1,10 +1,82 @@
 #import "Parser.h"
 
+
+
+
 @implementation Parser
+
+-(id)init {
+    self = [super init];
+    ringScopes = @[@"(", @")"];
+    triangleScopes = @[@"<", @">"];
+    rectangleScopes = @[@"[", @"]"];
+    return self;
+}
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    return @[@"Test"];
+    NSMutableArray <NSString*> *result = [[NSMutableArray alloc] init];
+    
+    NSString *currentSymbol;
+    NSUInteger len = 0;
+    NSString* stringForSearch;
+    for(NSUInteger index = 0; index < [string length]; index+=1) {
+        currentSymbol = [string substringWithRange: NSMakeRange(index, 1)];
+        
+        if([currentSymbol isEqualToString:[ringScopes firstObject]]) {
+            
+            stringForSearch = [string substringFromIndex:index+1];
+            len = [Parser searchScopes:ringScopes inString:stringForSearch];
+            if(len != 0){
+                [result addObject:[stringForSearch substringWithRange:NSMakeRange(0, len)]];
+                continue;
+            }
+            
+        } else if ([currentSymbol isEqualToString:[triangleScopes firstObject]]) {
+            
+            stringForSearch = [string substringFromIndex:index+1];
+            len = [Parser searchScopes:triangleScopes inString:stringForSearch];
+            if(len != 0){
+                [result addObject:[stringForSearch substringWithRange:NSMakeRange(0, len)]];
+                continue;
+            }
+        } else if ([currentSymbol isEqualToString:[rectangleScopes firstObject]]) {
+        
+            stringForSearch = [string substringFromIndex:index+1];
+            len = [Parser searchScopes:rectangleScopes inString:stringForSearch];
+            if(len != 0){
+                [result addObject:[stringForSearch substringWithRange:NSMakeRange(0, len)]];
+                continue;
+            }
+        }
+            
+    }
+        
+    return result;
+}
+
++(NSInteger)searchScopes:(NSArray<NSString *> *)scopes inString:(NSString *)string {
+    NSUInteger repeat = 0;
+    NSString *currentSymbol = [NSString new];
+    
+    for(NSInteger index = 0; index < [string length]; index += 1){
+        currentSymbol = [string substringWithRange: NSMakeRange(index, 1)];
+        if([currentSymbol isEqualToString:[scopes firstObject]]){
+            repeat += 1;
+            continue;
+        }
+        if ([currentSymbol isEqualToString: [scopes lastObject]])  {
+            if(repeat != 0){
+                repeat -= 1;
+                continue;
+            } else {
+                return index;
+            }
+        }
+    }
+    return 0;
 }
 
 @end
+
+
